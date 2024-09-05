50f94b4cd53ba43cdc08b53fe327eb448129c199
nem <canhnamtran216@gmail.com>
2024-09-05 23:56:48 +0700

    init map & player

---
+++ scripts/pellet.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var noise: NoiseTexture3D
+var time_passed
+var origin_pos
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	time_passed = 0
+	origin_pos = global_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_passed += delta
+	var sample_noise = noise.noise.get_noise_1d(time_passed)
+
+	position.x = origin_pos.x + sample_noise * 5
+	position.y = origin_pos.y + sample_noise * 10
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("Scored")
+	queue_free()
