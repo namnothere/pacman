cda4f2376a8a986fa5be00fc6e0299bad3625e26
nem <canhnamtran216@gmail.com>
2024-10-22 14:59:46 +0700

    feat: maze generate

--- scripts/auto_grid_map.gd
+++ scripts/auto_grid_map.gd
@@ -1,30 +1,20 @@
 extends Node3D
 
-#var grid = [
-	#[1, 1, 1, 1, 1, 1, 1],
-	#[1, 0, 1, 0, 1, 1, 1],
-	#[1, 1, 0, 0, 1, 0, 1],
-	#[1, 1, 1, 1, 0, 1, 1],
-	#[1, 1, 0, 1, 1, 0, 1],
-	#[1, 0, 1, 0, 1, 1, 1],
-	#[1, 1, 1, 1, 1, 1, 1]
-#]
-
 var grid: Array = []
 var center_x: int
 var center_y: int
+var DIRECTIONS = [Vector2(0, -2), Vector2(0, 2), Vector2(-2, 0), Vector2(2, 0)]
 
-@export var MAP_SIZE: int = 21
+@export var MAP_SIZE: int = 25
 @onready var grid_map: GridMap = $GridMap_Maze
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	grid_map.clear()
 	init_map()
+	dfs_maze_generate()
 	generate_grid()
-	var cells = grid_map.get_used_cells_by_item(0)
-	print(cells)
-
+	#var cells = grid_map.get_used_cells_by_item(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -32,31 +22,60 @@ func _process(_delta: float) -> void:
 	
 func init_map():
 	for i in range(MAP_SIZE):
-		grid.append([])
-		for j in range(MAP_SIZE):
-			grid[i].append(0)
-			
-	center_x = MAP_SIZE / 2
-	center_y = MAP_SIZE / 2
+		var _row: Array = []
+		_row.resize(MAP_SIZE)
+		_row.fill(1)
+		grid.append(_row)
 
-	grid[center_y][center_x] = 1
+	center_x = MAP_SIZE / 2 + 1
+	center_y = MAP_SIZE / 2 + 1
 
-	print("Center of the array is mapped to (0, 0)")
-	
-	for row in grid:
-		print(row)
-	
+	grid[center_y][center_x] = 0
+
 func generate_grid():
 	for y in range(MAP_SIZE):
 		for x in range(MAP_SIZE):
 			var logical_x = x - center_x
 			var logical_y = y - center_y
 
-			if y == 0 or y == MAP_SIZE - 1 or x == 0 or x == MAP_SIZE - 1:
-				grid[y][x] = 1
+			if grid[y][x] == 1:
 				var logical_vector = Vector3i(logical_x, 0, logical_y)
 				grid_map.set_cell_item(logical_vector, 0)
-			else:
-				grid[y][x] = 0
-				
-			print("Array index: [", x, ",", y, "] -> Logical coordinates: (", logical_x, ",", logical_y, ")")
+
+func dfs_maze_generate(start_x: int = 0, start_y: int = 0):
+	grid[start_x][start_y] = 0
+	var stack = [Vector2(start_x, start_y)]
+
+	while stack.size() > 0:
+		var current: Vector2 = stack[stack.size() - 1]
+		var current_x: int = current.x
+		var current_y: int = current.y
+		
+		var neighbors = []
+		for direction in DIRECTIONS:
+			var nx = current_x + direction.x
+			var ny = current_y + direction.y
+			
+			if is_valid_move(nx, ny):
+				var between_x = current_x + direction.x / 2
+				var between_y = current_y + direction.y / 2
+				if grid[ny][nx] == 1 and grid[between_y][between_x] == 1:
+					neighbors.append(Vector2(nx, ny))
+			
+		if neighbors.size() > 0:
+			var next = neighbors[randi_range(0, neighbors.size() - 1)]
+			var next_x = next.x
+			var next_y = next.y
+
+			var between_x = current_x + (next_x - current_x) / 2
+			var between_y = current_y + (next_y - current_y) / 2
+
+			grid[between_y][between_x] = 0
+			grid[next_y][next_x] = 0
+			stack.append(next)
+		else:
+			stack.pop_back()
+	#print(grid)
+
+func is_valid_move(x: int, y: int):
+	return x >= 0 and x < MAP_SIZE and y >= 0 and y < MAP_SIZE and grid[y][x] == 1
